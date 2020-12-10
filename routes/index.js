c1fd868819aef96fb41412c99210800e4cbe08ca
Jey133 <jey133@gmail.com>
2020-12-11 00:16:20 +0200

    add css, bootstrap, jquery

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,17 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
 
-  request.get("https://reqres.in/api/users", (err, response, body) => {
+  request.get("https://reqres.in/api/users?page=1", (err, response, body) => {
     if (err) {
         return next(err);
     }
     console.log(JSON.parse(body));
     data = JSON.parse(body).data;
-    // res.send(data);
     res.render('index', { data: data });
-});
-
+  });
 });
 
 module.exports = router;
